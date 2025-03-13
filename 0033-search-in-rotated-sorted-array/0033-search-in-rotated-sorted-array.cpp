b2b2da45ff9d9149296547fd0108558a8b27e538
Shivam Kumar <130701188+ShivamKumar2883@users.noreply.github.com>
2025-03-13 10:37:41 +0530

    Time: 0 ms (100%), Space: 15.1 MB (66.14%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,24 +6,23 @@ public:
         while (left <= right) {
             int mid = left + (right - left) / 2;
             
-            if (nums[mid] == target) return mid; // Found target
+            if (nums[mid] == target) return mid; 
             
-            // Determine which half is sorted
-            if (nums[left] <= nums[mid]) { // Left half is sorted
+            if (nums[left] <= nums[mid]) { 
                 if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1; // Search in left half
+                    right = mid - 1; 
                 } else {
-                    left = mid + 1; // Search in right half
+                    left = mid + 1; 
                 }
-            } else { // Right half is sorted
+            } else { 
                 if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1; // Search in right half
+                    left = mid + 1; 
                 } else {
-                    right = mid - 1; // Search in left half
+                    right = mid - 1; 
                 }
             }
         }
         
-        return -1; // Target not found
+        return -1; 
     }
 };
